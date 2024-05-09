8f17b3b37557503dbc1db109b225ac90de7dedec
Ambi <ptlc.ptlc.ptlc@gmail.com>
2024-05-09 18:05:57 +0200

    refactor building of vehicle

---
+++ static/scripts/remote.js
@@ -0,0 +1,132 @@
+//const State = Object.assign({}, ...Object.entries(["BUILD", "WAIT", "PLAY", "SPECTATE"]).map(([i, s]) => ({ [s]: i })));
+
+class Remote {
+
+    constructor(url) {
+        this.selfPlayer = undefined;
+        this.state = State.BUILD;
+        this.nextTagId = 0;
+        this.listeners = { open: [], close: [], message: [], messageerror: [] };
+        this.ws = new WebSocket(url);
+        this.ws.onopen = this.onOpen.bind(this);
+        this.ws.onmessage = this.onMessage.bind(this);
+        this.ws.onclose = this.onClose.bind(this);
+        this.ws.onerror = this.onError.bind(this);
+    }
+
+    addEventListener(event, listener) {
+        this.listeners[event].push(listener);
+    }
+
+    removeEventListener(event, listener) {
+        this.listeners[event].splice(this.listeners[event].indexOf(listener), 1);
+    }
+
+    onOpen(event) {
+        console.log("[ws] Connecté");
+        this.send("setname", { name: "myself" });
+        if (this.listeners.open)
+            this.listeners.open(this);
+    };
+
+    onMessage(event) {
+        let args = JSON.parse(event.data);
+        if (args.command) {
+            if (this.listeners.message)
+                this.listeners.message(this, args.command, args);
+        } else if (args.error) {
+            if (this.listeners.messageerror)
+                this.listeners.messageerror(this, args.error, args);
+        }
+    }
+
+    onCommand(command, args) {
+        console.group("[ws] Serveur : " + command);
+        console.info(args);
+        console.groupEnd();
+        if (command == "logged") { // self, selfId
+            // Lorsque le joueur est enregistré
+            this.selfPlayer = Player.cast(args.self);
+            this.selfPlayer.id = args.selfId;
+        } else if (command == "start") { // map, vehiclesPatterns, opponents, gamemode
+            // Lorsqu'une instance de jeu démarre
+            var map = WorldMap.cast(args.map);
+            game = new Game(map, Gamemodes.getByName(args.gamemode), args.vehiclesPatterns, args.opponents);
+            engine.setCameraSize(20);
+            state = State.PLAY;
+            game.start();
+        } else if (command == "spectate") { // map, vehiclesPatterns, opponents, events, tick
+            // Lorsque l'on devient spectateur d'un jeu
+            var map = WorldMap.cast(args.map);
+            game = new Game(map, Gamemodes.getByName(args.gamemode), args.vehiclesPatterns, args.opponents);
+            game.events = args.events;
+            game.world.tick = args.tick;
+            engine.setCameraSize(20);
+            state = State.SPECTATE;
+            game.start();
+            game.regenerate();
+        } else if (command == "gameevent") { // event
+            // Lorqu'un évent se produit lors d'un match, ex : activate, disactivate
+            this.game.insertEvent(args.event.index, args.event.tick, args.event)
+        } else if (command == "wait") {
+            this.state = State.WAIT;
+        } else if (command == "leavequeue") {
+            state = State.BUILD;
+        } else {
+            return console.error("[ws] Unknow command : " + command);
+        }
+        if (this.listeners[command])
+            for (let listener of this.listeners[command])
+                listener(this, args);
+    }
+
+    onClose(event) {
+        console.log("[ws] Déconnecté");
+        if (this.listeners.close)
+            this.listeners.close(this);
+    }
+
+    onError(event) {
+        console.group("[ws] Erreur : " + error);
+        console.warn(args);
+        console.groupEnd();
+    }
+
+    send(command, args = {}) {
+        args.command = command;
+        this.ws.send(JSON.stringify(args));
+    }
+
+    createTag() {
+        return selfPlayer.id + "#" + (this.nextTagId++);
+    }
+
+    startSolo(vehiclePattern) {
+        this.send("startsolo", { vehiclePattern });
+    }
+
+    startMatch(vehiclePattern) {
+        this.send("startmatch", { vehiclePattern });
+    }
+
+    leaveQueue() {
+        this.send("leavequeue");
+    }
+
+    activate(index) {
+        let tag = createTag();
+        this.send("activate", { index: index, tag: tag });
+        game.activate(selfPlayer.id, index, tag, true);
+    }
+
+    disactivate(index) {
+        let tag = createTag();
+        this.send("disactivate", { index: index, tag: tag });
+        game.disactivate(selfPlayer.id, index, tag, true);
+    }
+
+    spectate(playerId) {
+        this.send("spectate", { playerId: playerId });
+        spectatedPlayerId = playerId;
+    }
+}
---
+++ static/scripts/renderer.js
@@ -0,0 +1,151 @@
+"use strict";
+
+class Renderer {
+
+    constructor(canvas, game) {
+        this.canvas = canvas;
+        this.game = game;
+    }
+
+    /**
+     * Affiche le jeu
+     * @param {WorldContext} wctx 
+     * @param {RenderContext} rctx 
+     * @param {number} rendererRatio 
+     */
+    render(wctx, rctx, rendererRatio) {
+        wctx.clear();
+        wctx.setZ(-2);
+        wctx.drawRectInfiniteX("#6ab9e2", -2176, 242);
+        wctx.drawImageInfiniteX(getImage("sky"), 0, -128, 128, 72);
+        wctx.drawRectInfiniteX("#b4d5f4", -56, 2048);
+        wctx.setZ(0);
+        // Affichage du sol
+        if (this.game && this.game.map)
+            this.renderMap(wctx);
+        if ((state == State.PLAY || state == State.SPECTATE) && this.game!==undefined)
+            this.renderInGame(wctx, rctx, rendererRatio);
+        if (state == State.BUILD || state == State.WAIT)
+            this.renderVehicleEditor(wctx, rctx, rendererRatio);
+        if (state == State.BUILD)
+            this.renderVehicleBuilder(wctx, rctx, rendererRatio);
+        if (state == State.WAIT)
+            this.renderWait(rctx, rendererRatio);
+    }
+
+    /**
+     * Affiche la carte
+     * @param {WorldContext} wctx
+    */
+    renderMap(wctx) {
+        wctx.drawLines("green", this.game.map.groundVertices.map(e=>e[0]), this.game.map.groundVertices.map(e=>e[1]), .1);
+    }
+
+    /**
+     * Affiche le jeu en cours
+     * @param {WorldContext} wctx 
+     * @param {RenderContext} rctx 
+     * @param {number} rendererRatio 
+     */
+    renderInGame(wctx, rctx, rendererRatio) {
+        // Centrage de la caméra sur le véhicule du joueur
+        let playerIdToFollow = state==State.PLAY ? selfPlayer.id : state==State.SPECTATE ? spectatedPlayerId : undefined;
+        let opponentIndexToFollow = this.game.getPlayerIndex(playerIdToFollow);
+        if (this.game.world.vehicles[opponentIndexToFollow] !== undefined) {
+            let toFollow = this.game.world.vehicles[opponentIndexToFollow].pos;
+            engine.setCameraPos(toFollow?toFollow.get_x():0, toFollow?toFollow.get_y():0);
+        }
+        // Affichage des arrivées
+        for (let finish of this.game.map.finishes) {
+            wctx.drawImage(getImage("finish"), spawn[0]-1, spawn[1]-2, 2, 4);
+        }
+        for (const [index,vehicle] of Object.entries(this.game.world.vehicles)) {
+            // Véhicules
+            for (let line of vehicle.parts) for (let part of line) {
+                if (part == undefined) continue;
+                wctx.drawImage(getImage(part.id+(part.color!=undefined?part.color:"")), part.body.GetPosition().get_x(), part.body.GetPosition().get_y(), 1, 1, part.body.GetAngle());
+                if (part.contained)
+                    wctx.drawImage(getImage(part.contained.id), part.body.GetPosition().get_x(), part.body.GetPosition().get_y(), .9, .9, part.body.GetAngle());
+                if (part.id == "player" || (part.contained && part.contained.id == "player"))
+                    wctx.drawText("Joueur "+this.game.opponents[index], part.body.GetPosition().get_x(), part.body.GetPosition().get_y()-1, .5, "white", 0, "black", "center");
+            }
+            if (index==opponentIndexToFollow) {
+                // Affichage des contrôles du véhicule du joueur
+                for (let i = 0; i < vehicle.controls.length; i++) {
+                    rctx.drawRect("#A0A0A0", i*40-(20*vehicle.controls.length)+20+2, 80+2, 30, 30);
+                    rctx.drawRect("#C0C0C0", i*40-(20*vehicle.controls.length)+20, 80, 30, 30);
+                    rctx.drawImage(getImage(vehicle.controls[i].id+vehicle.controls[i].color), i*40-(20*vehicle.controls.length)+20, 80, 20, 20, vehicle.controls[i].rotation*Math.PI/2);
+                    rctx.drawText(vehicle.controls[i].parts[0].activated?"ON":"OFF", i*40-(20*vehicle.controls.length)+8, 86, 12, "white", 0, "black");
+                    if (i<ControlKeys.length) rctx.drawText(ControlKeys[i].display, i*40-(20*vehicle.controls.length)+34, 70, 12, "white", 0, "black", "right");
+                }
+            }
+        }
+    }
+
+    /**
+     * Affiche l'éditeur de véhicule
+     * @param {WorldContext} wctx
+     * @param {RenderContext} rctx
+     * @param {number} rendererRatio
+     */
+    renderVehicleEditor(wctx, rctx, rendererRatio) {
+        for (let i = 0; i < vehiclePattern.length; i++)
+            for (let j = 0; j < vehiclePattern[i].length; j++) {
+                wctx.drawRect("#D0D0D0", j*1.2-(.6*vehiclePattern[i].length)+.6, (i-vehiclePattern.length+1)*1.2-.6, 1, 1);
+                if (vehiclePattern[i][j]) {
+                    let angle = 0;
+                    if (vehiclePattern[i][j].rotation) angle = (vehiclePattern[i][j].rotation||0)*Math.PI/2;
+                    else if (vehiclePattern[i][j].rotate4attachable) angle = (vehiclePattern[i][j].rotationattachable||0)*Math.PI/2;
+                    wctx.drawImage(getImage(vehiclePattern[i][j].id+(vehiclePattern[i][j].color??"")), j*1.2-(.6*vehiclePattern[i].length)+.6, (i-vehiclePattern.length+1)*1.2-.6, 1, 1, angle);
+                    if (vehiclePattern[i][j].contained!=undefined)
+                        wctx.drawImage(getImage(vehiclePattern[i][j].contained.id+(vehiclePattern[i][j].contained.color ?? "")), j*1.2-(.6*vehiclePattern[i].length)+.6, (i-vehiclePattern.length+1)*1.2-.6, .9, .9, angle);
+                    if (vehiclePattern[i][j].rotate4attachable) {
+                        wctx.drawImage(getImage("rotation-arrow"), j*1.2-(.6*vehiclePattern[i].length)+.6, (i-vehiclePattern.length+1)*1.2-.6, 1, 1, ((vehiclePattern[i][j].rotationattachable||0)-1)*Math.PI/2);
+                    }
+                }
+            }
+    }
+
+    /**
+     * Affiche la contruction de véhicule
+     * @param {WorldContext} wctx
+     * @param {RenderContext} rctx
+     * @param {number} rendererRatio
+     */
+    renderVehicleBuilder(wctx, rctx, rendererRatio) {
+        // Barre d'inventaire
+        if (selfPlayer && selfPlayer.inventory) {
+            let inventory = Object.entries(selfPlayer.inventory).map(e=>({id:e[0],amount:e[1].amount}));
+            for (let i = 0; i < inventory.length; i++) {
+                let x = i%9*40-20*Math.min(9, inventory.length);
+                let y = 80-40*Math.floor(i/9);
+                rctx.drawRect("#A0A0A0", x+22, y+2, 30, 30);
+                rctx.drawRect("#C0C0C0", x+20, y, 30, 30);
+                rctx.drawImage(getImage(inventory[i].id), x+20, y, 20, 20)
+                rctx.drawText(inventory[i].amount, x+8, y+6, 12, "white", 0, "black");
+            }
+        }
+        // Objet en placement
+        if (placingItem) {
+            rctx.drawImage(getImage(placingItem.id), mousePos.rx, mousePos.ry, 20, 20);
+            if (placingItem.contained) rctx.drawImage(getImage(placingItem.contained.id), mousePos.rx, mousePos.ry, 18, 18);
+        }
+        // Boutons start
+        rctx.drawImage(getImage("solo"), 120, 0, 20, 20);
+        rctx.drawImage(getImage("duo"), 120, -30, 20, 20);
+        rctx.drawImage(getImage("spectate"), -120, -30, 20, 20);
+    }
+
+    /**
+     * Affiche l'attente de match
+     * @param {RenderContext} rctx
+     * @param {number} rendererRatio
+     */
+    renderWait(rctx, rendererRatio) {
+        rctx.drawImage(getImage("quit"), 120, 0, 20, 20);
+        let loadDot = "   ";
+        loadDot[parseInt(Date.now()/1000)%3] = ".";
+        rctx.drawText("Recherche d'adversaires en cours"+loadDot, 0, 60, 20, "white", 0, "black", "center");
+    }
+
+}
