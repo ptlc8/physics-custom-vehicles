2c1466627544e5e35a2c3711b57f99958f4813c0
Ambi <61095624+ptlc8@users.noreply.github.com>
2025-02-20 15:26:15 +0100

    fix

--- server/pcvServer.js
+++ server/pcvServer.js
@@ -101,7 +101,7 @@ const dirname = path.dirname(fileURLToPath(import.meta.url));
 const require = createRequire(import.meta.url);
 fs.readdirSync(path.join(dirname, "commands"))
 	.forEach(file => {
-		PcvServer.commands[path.substring(0, path.lastIndexOf("."))] = require(path.join(dirname, 'commands', file));
+		PcvServer.commands[file.substring(0, file.lastIndexOf("."))] = require(path.join(dirname, 'commands', file));
 	});
 
 console.info("[pcv] Commandes chargées: " + Object.keys(PcvServer.commands).join(", "))
