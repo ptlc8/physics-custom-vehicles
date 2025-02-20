a1de7e4fac3dbac579d5529070e851e519a5383a
Ambi <61095624+ptlc8@users.noreply.github.com>
2025-02-20 11:52:28 +0100

    fix

--- server/pcvServer.js
+++ server/pcvServer.js
@@ -98,8 +98,9 @@ import { createRequire } from 'node:module';
 
 PcvServer.commands = {};
 
-const require = createRequire(import.meta.url);
-fs.readdirSync(path.join(path.basename(import.meta.url), "commands"))
+const dirname = path.basename(import.meta.url);
+const require = createRequire(dirname);
+fs.readdirSync(path.join(dirname, "commands"))
 	.forEach(file => {
 		PcvServer.commands[path.basename(file)] = require(path.join(__dirname, 'commands', file));
 	});
